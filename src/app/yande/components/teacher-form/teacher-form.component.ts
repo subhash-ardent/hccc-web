ee57eaf97129ba8d1b391aa8f2515bad29db3944
subash-ardent <subash.m@ardent-india.com>
2018-10-11 18:18:34 +0530

    teacherForm validations and toolTips

--- src/app/yande/components/teacher-form/teacher-form.component.ts
+++ src/app/yande/components/teacher-form/teacher-form.component.ts
@@ -1,5 +1,10 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ViewChild } from '@angular/core';
 import { Teacher } from '../../models/teacher';
+import { NgForm } from '@angular/forms';
+import { YandeApiService } from '../../services/yande-api.service';
+import { SnackBarService } from '../../../core/services/snack-bar.service';
+import { Account } from '../../../core/models/account';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'hccc-teacher-form',
@@ -10,10 +15,51 @@ export class TeacherFormComponent implements OnInit {
   @Input() model = new Teacher();
   @Input() tAction: string = '';
   @Input() hideTDetails:boolean;
+  @ViewChild('teacherForm') teacherForm: NgForm;
 
-  constructor() { }
+  devotee: Account;
+  isTeacher = false;
+  invalidPhoneNumber = false;
+
+  constructor(
+    private apiService: YandeApiService,
+    private snachBarService: SnackBarService,
+    private router: Router
+    ) { }
 
   ngOnInit() {
+  }
+
+  getDevotee(value: string) {
+    // mock backend has data for phone numbers 5555555555, 6666666666 and 7777777777
+    if (value && value.length >= 10) {
+      this.invalidPhoneNumber = false;
+      this.apiService.getDevotee(value).subscribe(a => {
+        if (a.length && a.length > 0) {
+          this.devotee = a[0];
+          if (this.devotee && this.devotee.roles && this.devotee.roles.includes('Teacher')) {
+            this.isTeacher = true;
+          }
+        } else {
+          this.snachBarService.showFailureSnackBar(
+            'The phone number ' + value + ', is not associated with any devotee in the system'
+          );
+        }
+      });
+    } else {
+      this.invalidPhoneNumber = true;
+    }
+
+  }
+
+
+
+  onSubmit(){
+    console.log("submitted");
+  }
+  onCancelling(){
+    this.teacherForm.reset();
+    this.router.navigate(['/teachers']);
   }
 
 }
